b8c53095c171c87d2b746a17c5c173bb0413fdd0
Illia Kornyk <krekerbrow@gmail.com>
2026-03-07 11:33:57 +0200

    refactor: snippet controller

---
+++ backend/src/snippets/snippets.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Snippet, SnippetSchema } from './schemas/snippet.schema';
+import { SnippetsController } from './snippets.controller';
+import { SnippetsService } from './snippets.service';
+
+@Module({
+  imports: [MongooseModule.forFeature([{ name: Snippet.name, schema: SnippetSchema }])],
+  controllers: [SnippetsController],
+  providers: [SnippetsService],
+})
+export class SnippetsModule {}
